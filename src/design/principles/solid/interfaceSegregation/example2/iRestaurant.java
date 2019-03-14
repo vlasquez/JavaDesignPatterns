3ef5d5906656da3cfb2055d0738abf5521854039
Andres Velasquez <VLASQUEZ53069@HOTMAIL.COM>
2019-03-14 11:33:31 -0500

    Comments for make a successful Liskov substitution

--- src/design/principles/solid/interfaceSegregation/example2/iRestaurant.java
+++ src/design/principles/solid/interfaceSegregation/example2/iRestaurant.java
@@ -1,7 +1,17 @@
 package design.principles.solid.interfaceSegregation.example2;
 
 public interface iRestaurant {
-
+    /** Para implementar correctamente el principio de sustitucion de Liskov se deben crear:
+     *
+     * Una interface llamada iOrder con los metodos:
+     * acceptOnlineOrder()
+     * takeTelephoneOrder()
+     * walkInCostumerOrder()
+     *
+     * Otra interface llamada iPayment con los metodos:
+     * payOnline()
+     * payInPerson()
+     */
     public void acceptOnlineOrder();
     public void takeTelephoneOrder();
     public void payOnline();
