919ae2a02da5367d8abf3f2e5faa40601243a2ef
Andres Velasquez <VLASQUEZ53069@HOTMAIL.COM>
2019-04-21 17:22:11 -0500

    Car models for prototype challenge

--- src/design/patterns/creationals/prototype/challenge/car/Ford.java
+++ src/design/patterns/creationals/prototype/challenge/car/Ford.java
@@ -1,8 +1,19 @@
 package design.patterns.creationals.prototype.challenge.car;
 
+import design.patterns.creationals.prototype.challenge.BasicCar;
+
 /**
  * @autor Andrés Velasquez
  * @since 2019-04-21
  **/
-public class Ford {
+public class Ford  extends BasicCar {
+
+    public Ford(String model) {
+        modelName = model;
+    }
+
+    @Override
+    public BasicCar clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
--- src/design/patterns/creationals/prototype/challenge/car/Renault.java
+++ src/design/patterns/creationals/prototype/challenge/car/Renault.java
@@ -1,8 +1,19 @@
 package design.patterns.creationals.prototype.challenge.car;
 
+import design.patterns.creationals.prototype.challenge.BasicCar;
+
 /**
  * @autor Andrés Velasquez
  * @since 2019-04-21
  **/
-public class Renault {
+public class Renault extends BasicCar {
+
+    public Renault(String model) {
+        modelName = model;
+    }
+
+    @Override
+    public BasicCar clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
